960abdaed7ce13e9a7fcf8eb5200bc381b3be244
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-04-17 14:07:50 +0530

    Commited Add npm validator files

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,5 @@
+const validator = require('validator')
+
 // exporting function--->
 
 const getNotes = require('./notes.js');
@@ -5,3 +7,6 @@ const getNotes = require('./notes.js');
 let msg= getNotes();
 
 console.log(msg);
+ 
+
+console.log(validator.isURL("google.com"));
