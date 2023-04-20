fa7a3d019e1432e75afd838d6434664567c6d16a
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-04-20 16:03:13 +0530

    There is a property and that's where we can access all the command line arguments passed in to our app.

--- notes-app/app.mjs
+++ notes-app/app.mjs
@@ -11,3 +11,6 @@ console. log (validator. isURL("google.com")) ;
 
 let greenMsg = chalk.green.bold.italic.inverse('Success!');
 console.log(greenMsg);
+
+// there is a property and that's where we can access all the command line arguments passed in to our app .
+console.log(process.argv[2]);
