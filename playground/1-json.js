e2d6ff9bd4d56c9348d2aaf53688b68f30074c6b
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-04-25 03:00:48 +0530

    commited 1-json.json using fs.WriteFileSync('1-json.json', bookJSON)

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,12 +1,9 @@
+const fs = require("fs")
+
 const book = {
     title : 'Atomic Habits',
     author : 'James clear'
 };
 const bookJSON = JSON.stringify(book);
-console.log(bookJSON);
 
-// opposite of JSON.stringify , which takes in the JSON string and gives us back the object.
-const parse =  JSON.parse(bookJSON);
-console.log(parse);
-console.log(parse.title);
-console.log(parse.author);
+fs.writeFileSync("1-json.json", bookJSON)
