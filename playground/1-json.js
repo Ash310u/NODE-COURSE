9d5a033974338877d66379475872c5ccbca99662
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-04-25 03:24:17 +0530

    commited play with 1-json.json file

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,9 +1,28 @@
-const fs = require("fs")
+const { log } = require("console");
+const fs = require("fs");
 
-const book = {
-    title : 'Atomic Habits',
-    author : 'James clear'
-};
-const bookJSON = JSON.stringify(book);
+// const book = {
+//     title : 'Atomic Habits',
+//     author : 'James clear'
+// };
+// const bookJSON = JSON.stringify(book);
 
-fs.writeFileSync("1-json.json", bookJSON)
+// fs.writeFileSync("1-json.json", bookJSON)
+
+
+// Now we are going to read the content of "1-json.json" file in this file--->
+
+const dataBuffer = fs.readFileSync("1-json.json");
+
+// And it return a Buffer, which is a way for Node.js to represent Binary data
+console.log(dataBuffer);
+
+// Now if we actually want to get our string back , there is a toString method 
+const dataJson = dataBuffer.toString();
+console.log(dataJson);
+
+// parse the JSON data into a object
+const data = JSON.parse(dataJson)
+
+console.log(data.title);
+console.log(data.author);
