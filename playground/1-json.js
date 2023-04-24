01a3c3a6d6bbc82b1dd32533b735f102a0669592
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-04-25 03:56:52 +0530

    commited .gitignore ./playground

--- playground/1-json.js
+++ playground/1-json.js
@@ -29,9 +29,11 @@ const fs = require("fs");
 const dataBuffer = fs.readFileSync("1-json.json");
 const dataJSON = dataBuffer.toString()
 const parseData = JSON.parse(dataJSON);
+
 parseData.Name = 'Ash'
 parseData.Planet = 'Keplar 22b'
 parseData.Age = 19
+
 const newJsonData = JSON.stringify(parseData)
 const newdata = fs.writeFileSync("1-json.json", newJsonData )
 // console.log(newdata);
