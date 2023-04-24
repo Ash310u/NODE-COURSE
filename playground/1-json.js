df4c1bd3d7b8fa8754bb08f98f94a957da3cc6ef
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-04-25 03:52:21 +0530

    commited .gitignore ./playground

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,27 +1,37 @@
 const fs = require("fs");
 
-// const book = {
-//     title : 'Atomic Habits',
-//     author : 'James clear'
-// };
-// const bookJSON = JSON.stringify(book);
+// // const book = {
+// //     title : 'Atomic Habits',
+// //     author : 'James clear'
+// // };
+// // const bookJSON = JSON.stringify(book);
 
-// fs.writeFileSync("1-json.json", bookJSON)
+// // fs.writeFileSync("1-json.json", bookJSON)
 
 
-// Now we are going to read the content of "1-json.json" file in this file--->
+// // Now we are going to read the content of "1-json.json" file in this file--->
+
+// const dataBuffer = fs.readFileSync("1-json.json");
+
+// // And it return a Buffer, which is a way for Node.js to represent Binary data
+// console.log(dataBuffer);
+
+// // Now if we actually want to get our string back , there is a toString method 
+// const dataJson = dataBuffer.toString();
+// console.log(dataJson);
+
+// // parse the JSON data into a object
+// const data = JSON.parse(dataJson)
+
+// console.log(data.title);
+// console.log(data.author);
 
 const dataBuffer = fs.readFileSync("1-json.json");
-
-// And it return a Buffer, which is a way for Node.js to represent Binary data
-console.log(dataBuffer);
-
-// Now if we actually want to get our string back , there is a toString method 
-const dataJson = dataBuffer.toString();
-console.log(dataJson);
-
-// parse the JSON data into a object
-const data = JSON.parse(dataJson)
-
-console.log(data.title);
-console.log(data.author);
+const dataJSON = dataBuffer.toString()
+const parseData = JSON.parse(dataJSON);
+parseData.Name = 'Ash'
+parseData.Planet = 'Keplar 22b'
+parseData.Age = 19
+const newJsonData = JSON.stringify(parseData)
+const newdata = fs.writeFileSync("1-json.json", newJsonData )
+// console.log(newdata);
