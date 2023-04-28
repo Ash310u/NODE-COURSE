bc7bf61d6fc895213d8abba4acbeefc2a718a2bc
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-04-29 01:34:41 +0530

    commited added listNotes function on notes.js file and added a handler for the function on app.js file and the list command start working

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,60 +1,65 @@
-
 const fs = require('fs');
 
+let getNotes = () => 'your notes...';
 
-let getNotes = function() {
- return 'your notes...';
-}
-
-const addNote = function (title,body) {
-     const notes = loadNotes()
+const addNote =(title,body) => {
+     const notes = loadNotes();
      
-     const duplicateNotes = notes.filter(function (note) {
-          return note.title === title;
-     })
+     const duplicateNotes = notes.filter( note => note.title === title);
+
      if (duplicateNotes.length === 0) {
           notes.push({
                title,
                body
-          })
-          saveNotes(notes)    
+          });
+          saveNotes(notes);
           console.log('New Note Added!');
      }else{
           console.log('Note Title Taken!');
+     };
+};
+
+const removeNote = title => {
+     const notes = loadNotes();
+     
+     const notesToKeep = notes.filter( note => note.title !== title);
+     
+     if (notes.length > notesToKeep.length) {
+          console.log('Note removed!');
+          saveNotes(notesToKeep);
+     }else{
+          console.log(`No note found!`);
      }
+
+};
+
+const listNotes = () => {
+     const notes =  loadNotes()
+
+     console.log(`Your notes:`);
+     return notes.forEach(note => console.log(note.title));
 }
 
-const removeNote = function (title){
-     const notes = loadNotes()
+const saveNotes = notes => {
+     const dataJSON = JSON.stringify(notes);
+     fs.writeFileSync('notes.json', dataJSON);
      
-     const notesToKeep = notes.filter(function (note) {
-          return note.title !== title;
-     })
-     
-     saveNotes(notesToKeep);
+};
 
-}
-
-const saveNotes = function(notes){
-     const dataJSON = JSON.stringify(notes)
-     fs.writeFileSync('notes.json', dataJSON)
-     
-}
-
-const loadNotes = function () {
+const loadNotes = ()=> {
      try {
-          const dataBuffer = fs.readFileSync('notes.json')
-          const dataJSON = dataBuffer.toString()
+          const dataBuffer = fs.readFileSync('notes.json');
+          const dataJSON = dataBuffer.toString();
           return JSON.parse(dataJSON);
      } catch (e) {
           return [];
-     }
+     };
 }
 
 
 module.exports = {
      getNotes,
      addNote,
-     removeNote
+     removeNote,
+     listNotes
 };
-
