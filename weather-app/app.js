2030e955a66062ebb8f62af28cf1c7ec20f8a043
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-05-06 04:50:57 +0530

    commited pretty decent error handling setup by using if else

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,18 +1,25 @@
 const request = require("postman-request");
 
 
-const placeName = `New york`
-const GeocodeURL = `https://us1.locationiq.com/v1/search?key=pk.bbb941e8068fd9d57e6f8705a6c4d8ec&q=${placeName}&format=json&limit=1`;
+const placeName = `kolkata`
+const GeocodeURL = `https://us1.locationiq.com/v1/search?key=pk.bbb941e8068fd9d57e6f8705a6c4d8ec&format=json&q=${placeName}&limit=1`;
+
 
 request({ url: GeocodeURL, json: true }, (error, response) => {
-    const lat = response.body[0].lat
-    const lon = response.body[0].lon
+    if (error) {
+        console.log(`Unble to connect to weather service!`);
+    } else if (response.body.error) {
+        console.log(`Unble to find location`);
+    }else {
+        const lat = response.body[0].lat
+        const lon = response.body[0].lon
 
-    const ForecastURL = `http://api.weatherstack.com/current?access_key=11388a4160847cee46b22a078180de9e&query=${lat},${lon}&units=m`;
+        const ForecastURL = `http://api.weatherstack.com/current?access_key=11388a4160847cee46b22a078180de9e&query=${lat},${lon}&units=m`;
 
-    request({ url: ForecastURL, json: true }, (error, response) => {
-        const { feelslike, temperature, weather_descriptions } = response.body.current;
-        console.log(`At ${lat} Latitude & ${lon} Longitude, It's ${weather_descriptions}. It is currently ${temperature} degress outside. It feels like ${feelslike} degress outside.`);
-    })
+        request({ url: ForecastURL, json: true }, (error, response) => {
+            const { feelslike, temperature, weather_descriptions } = response.body.current;
+            console.log(`At ${lat} Latitude & ${lon} Longitude, It's ${weather_descriptions}. It is currently ${temperature} degress outside. It feels like ${feelslike} degress outside.`);
+        })
 
+    }
 })
