7ae784d59f318be6bb525e205428503a9c982f6c
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-05-06 04:59:18 +0530

    commited added a location variable and pass it on the main console.log string

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,7 @@
 const request = require("postman-request");
 
 
-const placeName = `kolkata`
+const placeName = `chatra birbhum`
 const GeocodeURL = `https://us1.locationiq.com/v1/search?key=pk.bbb941e8068fd9d57e6f8705a6c4d8ec&format=json&q=${placeName}&limit=1`;
 
 
@@ -11,6 +11,7 @@ request({ url: GeocodeURL, json: true }, (error, response) => {
     } else if (response.body.error) {
         console.log(`Unble to find location`);
     }else {
+        const location = response.body[0].display_name
         const lat = response.body[0].lat
         const lon = response.body[0].lon
 
@@ -18,7 +19,7 @@ request({ url: GeocodeURL, json: true }, (error, response) => {
 
         request({ url: ForecastURL, json: true }, (error, response) => {
             const { feelslike, temperature, weather_descriptions } = response.body.current;
-            console.log(`At ${lat} Latitude & ${lon} Longitude, It's ${weather_descriptions}. It is currently ${temperature} degress outside. It feels like ${feelslike} degress outside.`);
+            console.log(`At ${lat} Latitude & ${lon} Longitude this location is ${location}. It's ${weather_descriptions}. It is currently ${temperature} degress outside. It feels like ${feelslike} degress outside.`);
         })
 
     }
