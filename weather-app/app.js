be2fb2a56554acb34e454203318e5527d42eb153
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-05-08 03:04:40 +0530

    commited using process.argv[2] also if else we can use command line to search an address

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,19 +1,25 @@
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
-geocode("kolkata", (error, data) => {
-    if (error) {
-        console.log(error);
-    } else {
-        const {lat, lon, location} = data
-        forecast(lat, lon, location,(error, data) => {
-            if (error) {
-                console.log(error);
-            } else {
-                console.log(
-                    `At ${lat} Latitude & ${lon} Longitude this location is ${location}. It's ${data.weatherDescriptions}. It is currently ${data.temperature} degress outside. It feels like ${data.feelslike} degress outside.`
-                );
-            }
-        });
-    }
-});
+const address = process.argv[2]
+
+if (!address) {
+    console.log('Please provide an address');
+} else {
+    geocode(address, (error, data) => {
+        if (error) {
+            console.log(error);
+        } else {
+            const {lat, lon, location} = data
+            forecast(lat, lon, location,(error, data) => {
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log(
+                        `At ${lat} Latitude & ${lon} Longitude this location is ${location}. It's ${data.weatherDescriptions}. It is currently ${data.temperature} degress outside. It feels like ${data.feelslike} degress outside.`
+                    );
+                }
+            });
+        }
+    });   
+}
