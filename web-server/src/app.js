1e5fb8a11528981d19fe7dc20433e9726a6550f8
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-05-10 00:43:31 +0530

    commited express.js code

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,8 +2,24 @@ const express = require('express')
 
 const app = express()
 
+// app.com--->
 app.get('',(req,res) => {
     res.send('Hello Express!');
+})
+
+// app.com/help--->
+app.get('/help',(req,res) => {
+    res.send('help page!');
+})
+
+// app.com/about--->
+app.get('/about',(req,res) => {
+    res.send('about page');
+})
+
+// app.com/weather--->
+app.get('/weather',(req,res) => {
+    res.send('weather forecast page');
 })
 
 app.listen(3000,() => {
