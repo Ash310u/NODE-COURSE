966061f7b4eed9d7990757aff51589823efb678e
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-05-18 15:48:48 +0530

    commited pointing express to our custom directory using path.join

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,8 +3,10 @@ const express = require('express')
 
 const app = express()
 const publicDir = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname,'../templates')
 
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
 app.use(express.static(publicDir))
 
 app.get('',(req,res) => {
