bf419298426ec8fa2b6d617188d5259a8814091e
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-05-26 15:33:27 +0530

    Commited fetching data from the localhost:3000/weather?address=

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,7 @@
 console.log('client side js file is loaded');
+
+fetch('http://localhost:3000/weather?address=kolkata').then((response) => {
+    response.json().then((data) => {
+        console.log(data);
+    })
+})
