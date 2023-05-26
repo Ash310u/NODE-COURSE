c76a2e9d72fd4884d80770b092ec316a85a92d9b
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-05-26 15:41:44 +0530

    Commited fetching data from the localhost:3000/weather?address= using if else for errors

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -2,6 +2,11 @@ console.log('client side js file is loaded');
 
 fetch('http://localhost:3000/weather?address=kolkata').then((response) => {
     response.json().then((data) => {
-        console.log(data);
+        if (data.error) {
+            console.log(data.error);
+        } else {
+            console.log(data.forecast);
+            console.log(data.location);
+        }
     })
 })
