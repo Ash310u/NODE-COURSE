4caf4527b47455ef39907ee63e634a2679f32f2a
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-05-28 14:57:16 +0530

    commited search is a representation of input and in search.value the value extracts the input value

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -12,9 +12,12 @@ fetch('http://localhost:3000/weather?address=kolkata').then((response) => {
 })
 
 const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
 
 weatherForm.addEventListener('submit' , (e) => {
     e.preventDefault()
+
+    const location = search.value
     
-    console.log('somthing')
+    console.log(location)
 }) 
