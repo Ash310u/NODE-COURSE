c581a7da3fc34614ce7a6febaa807649f7bf5961
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-05-28 15:06:56 +0530

    Migrate the fetch call into submit callback

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,23 +1,22 @@
 console.log('client side js file is loaded');
 
-fetch('http://localhost:3000/weather?address=kolkata').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error);
-        } else {
-            console.log(data.forecast);
-            console.log(data.location);
-        }
-    })
-})
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 
-weatherForm.addEventListener('submit' , (e) => {
-    e.preventDefault()
+weatherForm.addEventListener('submit', (e) => {
+    // it's prevent the browser to be default
+    e.preventDefault();
 
     const location = search.value
-    
-    console.log(location)
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error);
+            } else {
+                console.log(data.forecast);
+                console.log(data.location);
+            }
+        })
+    });
 }) 
