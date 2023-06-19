af36c8045d76a8cedf0f1d457d74e09ef63c59a1
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-06-20 01:33:28 +0530

    Commited insertMany it's allow us to insert multiple document into a collection

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -14,15 +14,43 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     }
     const db = client.db(databaseName);
 
-    db.collection('users').insertOne({
-        name : 'ash',
-        age:19  
-    }, (error,result) => {
-        if (error) {
-            return console.log('Unable to insert user'); 
-        }
+    // insertOne it's allow us to insert a single document into a collection --> 
 
+    // db.collection('users').insertOne({
+    //     name : 'ash',
+    //     age:19  
+    // }, (error,result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user'); 
+    //     }
+
+    //     // result.ops this contains all of the documents that were inserted
+    //     console.log(result.ops);
+    // } )
+
+    // insertMany it's allow us to insert multiple document into a collection --> 
+    // it's take a Array of Objects
+    db.collection('users').insertMany([
+        {
+            name:'Ash',
+            age:20
+        },
+        {
+            name:'Yash',
+            age:19
+        },
+        {
+            name:'Andrew',
+            age:34
+        }
+    ],(error,result) => {
+        if (error) {
+            return console.log('Unble to insert users');
+        }
         // result.ops this contains all of the documents that were inserted
         console.log(result.ops);
-    } )
+    })
+
+
+
 })
