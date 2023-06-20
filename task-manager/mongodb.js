e798a6cc46e4eca60af68f202efb9e4c681cd32e
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-06-21 01:08:36 +0530

    Discovering ObjectID's methods

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,12 +1,19 @@
-// CRUD - create read update delete
+// CRUD - create read update delete  - - ->
+// using destructuring in require('mongodb') object
+const { ObjectID, MongoClient }=  require('mongodb');
 
-const mongodb = require('mongodb')
-
-const MongoClient =  mongodb.MongoClient
+console.log(ObjectID);
 
 
 const connectionURL = 'mongodb://localhost:27017'
 const databaseName = 'task-manager'
+// using new because ObjectID is constructor function
+const id = new ObjectID()
+
+console.log(id);
+console.log(id.id.length);
+console.log(id.getTimestamp());
+console.log(id.toHexString().length);
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error) {
@@ -32,47 +39,50 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     // insertMany it's allow us to insert multiple document into a collection --> 
     // it's take a Array of Objects
-    db.collection('users').insertMany([
-        {
-            name:'Ash',
-            age:20
-        },
-        {
-            name:'Yash',
-            age:19
-        },
-        {
-            name:'Andrew',
-            age:34
-        }
-    ],(error,result) => {
-        if (error) {
-            return console.log('Unble to insert users');
-        }
-        // result.ops this contains all of the documents that were inserted
-        console.log(result.ops);
-    })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name:'Ash',
+    //         age:20
+    //     },
+    //     {
+    //         name:'Yash',
+    //         age:19
+    //     },
+    //     {
+    //         name:'Andrew',
+    //         age:34
+    //     }
+    // ],(error,result) => {
+    //     if (error) {
+    //         return console.log('Unble to insert users');
+    //     }
+    //     // result.ops this contains all of the documents that were inserted
+    //     console.log(result.ops);
+    // })
 
     // adding a tasks collection --->
 
-    db.collection('tasks').insertMany([
-        {
-            description:'Do a commit today',
-            completed:true,
-        },
-        {
-            description:'Hit gym today',
-            completed:false,
-        },
-        {
-            description:'Learn somthing new today',
-            completed:true,
-        }
-    ],(error,result) => {
-        if (error) {
-            return console.log('Unable to insert tasks');
-        }
-        console.log(result.ops);
-    })
+    // db.collection('tasks').insertMany([
+    //     {
+    //         description:'Do a commit today',
+    //         completed:true,
+    //     },
+    //     {
+    //         description:'Hit gym today',
+    //         completed:false,
+    //     },
+    //     {
+    //         description:'Learn somthing new today',
+    //         completed:true,
+    //     }
+    // ],(error,result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert tasks');
+    //     }
+    //     console.log(result.ops);
+    // })
+
+
 
 })
