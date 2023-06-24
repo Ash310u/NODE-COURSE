b5f665f754cf010030a688736b85df10e61f7025
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-06-24 20:17:40 +0530

    Commited some bug in mongodb updateOne method

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,6 +1,6 @@
 // CRUD - create read update delete  - - ->
 // using destructuring in require('mongodb') object
-const { ObjectID, MongoClient }=  require('mongodb');
+const { ObjectID, MongoClient } = require('mongodb');
 
 const connectionURL = 'mongodb://localhost:27017'
 const databaseName = 'task-manager'
@@ -11,48 +11,16 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     }
     const db = client.db(databaseName);
 
-    // R - Read 
-    // just finding one document using 'findOne'
-    db.collection('users').findOne(
-        { _id: new ObjectID('6490b64931d155603859fac3') },
-        (error,user) => {
-            if (error) {
-                return console.log('Unable to fecth');
-            }
+    // U - Update
 
-            console.log(user);
+    db.collection('users').updateOne(
+        {
+            _id: '6490b64931d155603859fac4'
+        }, {
+        $set: {
+            name: 'Devi',
         }
+    },
+
     )
-
-    // finding multiple documents using 'find'
-    // find require's a cursor method - like - toArray,count blah blah
-    db.collection('users').find(
-        { age:19 }
-    ).toArray((error,users) => {
-        console.log(users);
-    })
-    
-    db.collection('users').find(
-        { age:19 }
-    ).count((error,count) => {
-        console.log(count);
-    })
-
-
-    // using findOne in tasks collection
-    db.collection('tasks').findOne(
-            {_id: new ObjectID('6490b64931d155603859fac6')},
-            (error,task) => {
-                if (error) {
-                    return console.log('Unable to fecth');
-                }
-                console.log(task);
-            }
-        )
-    // using find in tasks collection
-    db.collection('tasks').find(
-            {completed:true}
-        ).toArray((error,tasks) => {
-            console.log(tasks);
-        })
 })
