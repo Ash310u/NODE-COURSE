4b282346e0c31c24bcd06d2f3bb7dc9976b119de
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-06-25 04:17:41 +0530

    fixed that bug now i can update any object but i have to put ObjectID before the _id and now i can update the file

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -5,6 +5,7 @@ const { ObjectID, MongoClient } = require('mongodb');
 const connectionURL = 'mongodb://localhost:27017'
 const databaseName = 'task-manager'
 
+
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error) {
         return console.log('Unable to connect to database!');
@@ -13,14 +14,15 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     // U - Update
 
-    db.collection('users').updateOne(
-        {
-            _id: '6490b64931d155603859fac4'
-        }, {
-        $set: {
-            name: 'Devi',
-        }
-    },
 
-    )
+        db.collection('users').updateOne(         
+                {"_id": ObjectID('6490b64931d155603859fac4')}, // Filter
+                {$set:{"name": 'andrew'}} // Update
+            )
+            .then((result) => {
+                console.log(result);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
 })
