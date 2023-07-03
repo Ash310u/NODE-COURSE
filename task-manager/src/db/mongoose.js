71427d0d4166515ff78a1dd9158a702acb2a51f9
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-07-03 17:15:46 +0530

    commited some Mongoose validators

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -6,65 +6,51 @@ mongoose.connect('mongodb://localhost:27017/task-manager-api', {
 
 // User model - - >
 
-// const User = mongoose.model('User',{
-//     name:{
-//         type: String
-//     },
-//     age:{
-//         type: Number
-//     }
-// })
+const User = mongoose.model('User',{
+    name:{
+        type: String,
+        required: true 
+    },
+    age:{
+        type: Number,
+        required: true,
+        validate(value) {
+            if (value < 0) {
+                throw new Error ('Age must be a positive number')
+            }
+        }
+    }
+})
 
-// const me = new User({
-//     name:'Ash',
-//     age:19
-// })
+const me = new User({
+    name:'Rick',
+    age: -90
+})
 
-// me.save().then(() => {
-//     console.log(me);
-// }).catch((err) => {
-//     console.log(err);
-// });
+me.save().then(() => {
+    console.log(me);
+}).catch((err) => {
+    console.log(err);
+});
 
 // Tasks model - - >
 
-const Task = mongoose.model('Task',{
-    description:{
-        type:String
-    },
-    completed:{
-        type:Boolean
-    }
-})
+// const Task = mongoose.model('Task',{
+//     description:{
+//         type:String
+//     },
+//     completed:{
+//         type:Boolean
+//     }
+// })
 
-const task = new Task({
-    description:'Do a commit',
-    completed:false
-})
+// const task = new Task({
+//     description:'Do a commit',
+//     completed:false
+// })
 
-task.save().then(() => {
-    console.log(task);
-}).catch((err) => {
-    console.log(err);
-});
-
-//  finding some bug in mongodb compass
-const People = mongoose.model('People',{
-    description:{
-        type:String
-    },
-    completed:{
-        type:Boolean
-    }
-})
-
-const Person = new People({
-    description:'Do a commit',
-    completed:false
-})
-
-Person.save().then(() => {
-    console.log(Person);
-}).catch((err) => {
-    console.log(err);
-});
+// task.save().then(() => {
+//     console.log(task);
+// }).catch((err) => {
+//     console.log(err);
+// });
