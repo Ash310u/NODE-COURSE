7e7a352a7662f90e6932c5f5168c43b9ee3fecd1
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-07-04 02:03:06 +0530

    commited new feild password with some custom validators

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -2,43 +2,56 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 
 mongoose.connect('mongodb://localhost:27017/task-manager-api', {
-    useNewUrlParser:true
+    useNewUrlParser: true
 })
 
 // User model - - >
 
-const User = mongoose.model('User',{
-    name:{
+const User = mongoose.model('User', {
+    name: {
         type: String,
         required: true,
         // by using trim true its remove spaces before and after string
-        trim:true
+        trim: true
     },
-    email:{
-        type:String,
-        required:true,
-        trim:true,
-        lowercase:true,
-        validate(value){
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
             if (!validator.isEmail(value)) {
                 throw new Error('Email is invalid')
             }
         }
     },
-    age:{
+    password: {
+        type: String,
+        required: true,
+        minlength:7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes("password")) {
+                throw new Error(`Password cannot contain "password"`)
+            }
+        }
+    },
+    age: {
         type: Number,
-        default:0,
+        default: 0,
         validate(value) {
             if (value < 0) {
-                throw new Error ('Age must be a positive number')
+                throw new Error('Age must be a positive number')
             }
         }
     }
 })
 
 const me = new User({
-    email:'Rick137@gmail.com',
-    name:'Rick Sanchez', 
+    email: 'Morty137@gmail.com',
+    name: 'Morty',
+    age: 14,
+    password: '1234'
 })
 
 me.save().then(() => {
