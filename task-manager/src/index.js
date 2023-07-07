51d23695413187ca84327fc539149f16594b4c20
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-07-07 21:05:20 +0530

    testing post request on postman

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -4,6 +4,10 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
+app.post('/users',(req, res) => {
+    res.send('testing!')
+})
+
 app.listen(port, () => {
     console.log(`server is up on ${port}`);
 })
