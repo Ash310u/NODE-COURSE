ad72c0d84cf572c1da852fe0dfcf2d37eb2cd8ce
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-07-09 23:51:57 +0530

    Commited user.js file

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,64 +1,9 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 mongoose.connect('mongodb://localhost:27017/task-manager-api', {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
-
-// User model - - >
-
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        // by using trim true its remove spaces before and after string
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid')
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength:7,
-        trim: true,
-        validate(value) {
-            if (value.toLowerCase().includes("password")) {
-                throw new Error(`Password cannot contain "password"`)
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number')
-            }
-        }
-    }
-})
-
-// const me = new User({
-//     email: 'Morty137@gmail.com',
-//     name: 'Morty',
-//     age: 14,
-//     password: '1234'
-// })
-
-// me.save().then(() => {
-//     console.log(me);
-// }).catch((err) => {
-//     console.log(err);
-// });
 
 // Tasks model - - >
 
@@ -73,13 +18,3 @@ const Task = mongoose.model('Task',{
         default:false
     }
 })
-
-const task = new Task({
-    description:`              Wake up at 6 o'clock`,
-})
-
-task.save().then(() => {
-    console.log(task);
-}).catch((err) => {
-    console.log(err);
-});
