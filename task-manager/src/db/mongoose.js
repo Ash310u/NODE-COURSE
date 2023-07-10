9dffbf69231ad62e9d551bcfc0302de3b98ce4eb
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-07-11 04:19:37 +0530

    Commited some bug

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://localhost:27017/task-manager-api', {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useCreateIndex:true,
+    useUnifiedTopology: true,
 })
 
 // Tasks model - - >
--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,20 +1,19 @@
 const express = require('express')
 require('./db/mongoose')
-const User = require('./models/user')
+const User = require('./models/user');
 
 const app = express()
-
 const port = process.env.PORT || 3000
 
 app.use(express.json())
 
 app.post('/users',(req, res) => {
     const user = new User(req.body)
-
+    
     user.save().then(() => {
         res.send(user)
     }).catch(() => {
-    });
+    })
 })
 
 app.listen(port, () => {
