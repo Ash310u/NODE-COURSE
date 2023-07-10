41f0c90e482d3db4c66d7d2b2921477202cf92ba
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-07-11 04:27:15 +0530

    fixed the bug

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,21 +1,25 @@
-const express = require('express')
-require('./db/mongoose')
-const User = require('./models/user');
+const express = require("express");
+require("./db/mongoose");
+const User = require("./models/user");
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(express.json())
+app.use(express.json());
 
-app.post('/users',(req, res) => {
-    const user = new User(req.body)
-    
-    user.save().then(() => {
-        res.send(user)
-    }).catch(() => {
+app.post("/users", (req, res) => {
+  const user = new User(req.body);
+
+  user
+    .save()
+    .then(() => {
+      res.send(user);
     })
-})
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
 
 app.listen(port, () => {
-    console.log(`server is up on ${port}`);
-})
+  console.log(`server is up on ${port}`);
+});
