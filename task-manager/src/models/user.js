f54261261ab54919a5b0602363cd7d80e60e16fb
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-07-12 13:09:48 +0530

    commited some comments on user.js file

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require("validator")
 
-
+// In this file we are put the criteria for Users details and exports that
 const User = mongoose.model('User', {
     name: {
         type: String,
