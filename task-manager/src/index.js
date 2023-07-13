b5a4a6c7800f82ab9526c003dd18e1097592bd1a
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-07-14 01:37:50 +0530

    commited users resource reading endpoints

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -22,14 +22,37 @@ app.post("/users", (req, res) => {
         });
 });
 
-app.post("/tasks", (req,res) => {
+app.get("/users", (req, res) => {
+    User.find({})
+        .then((users) => {
+            res.send(users)
+        }).catch((err) => {
+            res.status(500).send()
+        });
+})
+app.get("/users/:id", (req, res) => {
+
+    const _id = req.params.id
+
+    User.findById(_id)
+        .then((user) => {
+            if (!user) {
+                return res.status(404).send()
+            }
+            res.send(user)
+        }).catch((err) => {
+            res.status(500).send()
+        });
+})
+
+app.post("/tasks", (req, res) => {
     const task = new Task(req.body);
 
     task.save()
         .then(() => {
             res.status(201).send(task)
         })
-        .catch((err) => { 
+        .catch((err) => {
             res.status(400).send(err);
         });
 });
